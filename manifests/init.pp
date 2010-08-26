a1913beba6c78deee428f3e469bda788703dec56
Ahmed El Gamil <ahmed@manhag.org>
2010-08-26 09:27:33 +0200

    Almost finished installation and creating the asterisk DB

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,8 +19,22 @@ class asterisk {
 				}				
 # Install asterisk dependcies
 				$asterisk_packages = ["asterisk$version", "asterisk$version-configs", "asterisk$version-voicemail", "dahdi-linux", "dahdi-tools", "libpri"]
+				$mysql_packages = [ "mysql-server", "mysql" ]
 				package { $asterisk_packages: ensure => installed }
+				package { $mysql_packages: ensure => installed }
 			}				
 		}
 	}
+
+	class mysql_config ($root_password, $asterisk_db="asterisk", $asterisk_db_user="asterisk", $asterisk_db_password="redhat") {
+		# Take root password and create the asterisk DB
+		# Create the Asterisk Tables
+		exec { "asterisk_db" :
+			command => "mysql -u root -p$root_password -e 'CREATE DATABASE $asterisk_db; GRANT ALL PRIVILEGES ON $asterisk_db.* TO '$asterisk_db_user'@'localhost' IDENTIFIED BY '$asterisk_db_password' WITH GRANT OPTION;",
+			path	=> "/usr/bin:/usr/sbin:/bin"
+		}
 }
+
+}
+
+
