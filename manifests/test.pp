5e0a3c0695c24b2c4eba8dfc2510317995066b0e
Ahmed El Gamil <ahmed@manhag.org>
2010-08-26 04:52:05 +0200

    Deleted swap and old files

--- manifests/test.pp
+++
@@ -1,7 +0,0 @@
-file { "/etc/yum.repos.d/centos-asterisk.repo" :
-                 ensure => present,
-                 owner => root,
-                 group => root,
-                mode => 644,
-                source => "puppet:///centos-asterisk.repo"
-              }
