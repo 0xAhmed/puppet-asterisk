c9891de467525f570fd227b64541941437521903
Ahmed El Gamil <ahmed@manhag.org>
2010-09-10 08:43:20 +0200

    Almost finished, just some requires and befores and we are ready to go ..

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,33 +8,77 @@ class asterisk {
 					owner => root,
 					group => root,
 					mode => 644,
-					source => "puppet://$server/asterisk/centos-asterisk.repo"
+					source => "puppet://$server/asterisk/centos-asterisk.repo",
+					before => Package["$asterisk_packages"]
 				}
 				file { "/etc/yum.repos.d/centos-digium.repo" :
 					ensure => present,
 					owner => root,
 					group => root,
 					mode => 644,
-					source => "puppet://$server/asterisk/centos-digium.repo"
+					source => "puppet://$server/asterisk/centos-digium.repo",
+					before => Package["$asterisk_packages"]
 				}				
 # Install asterisk dependcies
-				$asterisk_packages = ["asterisk$version", "asterisk$version-configs", "asterisk$version-voicemail", "dahdi-linux", "dahdi-tools", "libpri"]
+				$asterisk_packages = ["asterisk$version", "asterisk$version-configs", "asterisk-addons-mysql"]
 				$mysql_packages = [ "mysql-server", "mysql" ]
 				package { $asterisk_packages: ensure => installed }
 				package { $mysql_packages: ensure => installed }
 			}				
 		}
+
+		service { "asterisk":
+			enable => true,
+			ensure => running,
+			hasrestart => "true",
+			hasstatus => "true",
+			require	=> Package["$asterisk_pacakges"],
+			alias => "asterisk"
+		}
+
+		
 	}
 
 	class mysql_config ($root_password, $asterisk_db="asterisk", $asterisk_db_user="asterisk", $asterisk_db_password="redhat") {
 		# Take root password and create the asterisk DB
 		# Create the Asterisk Tables
-		exec { "asterisk_db" :
-			command => "mysql -u root -p$root_password -e 'CREATE DATABASE $asterisk_db; GRANT ALL PRIVILEGES ON $asterisk_db.* TO '$asterisk_db_user'@'localhost' IDENTIFIED BY '$asterisk_db_password' WITH GRANT OPTION;",
-			path	=> "/usr/bin:/usr/sbin:/bin"
+		exec { "create_asterisk_db" :
+			command => "mysql -u root -predhat -e 'CREATE DATABASE $asterisk_db; GRANT ALL PRIVILEGES ON $asterisk_db.* TO '$asterisk_user'@'localhost' IDENTIFIED BY '$asterisk_db_password' WITH GRANT OPTION;'",
+			path	=> "/usr/bin:/usr/sbin:/bin",
+#			require => Class["install"]
 		}
-}
+
+		file { "/var/lib/mysql/asterisk.sql":
+			owner	=>	mysql,
+			group => 	mysql,
+			mode	=> 600,
+			content => template("asterisk/asterisk.sql.erb")
+		}
+
+		file { "/etc/asterisk/res_mysql.conf":
+			owner	=> asterisk,
+			group	=> asterisk,
+			mode => 664,
+			content	=> template("asterisk/res_mysql.conf.erb")
+		}
+
+		file { "/etc/asterisk/extconfig.conf":
+			owner	=> asterisk,
+			group	=> asterisk,
+			mode => 664,
+			content	=> template("asterisk/extconfig.conf.erb")
+		}
+
+		exec { "import_database" :
+			cwd => "/var/lib/mysql",
+			command => "mysql -u root -p$root_password $asterisk_db < asterisk.sql",
+			path => "/usr/bin:/usr/sbin:/bin",
+			require => File["/var/lib/mysql/asterisk.sql"],
+#			notify => Service["asterisk"]
+		}
+
+		
 
 }
-
+}
 
