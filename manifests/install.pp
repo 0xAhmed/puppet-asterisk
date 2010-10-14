bf5a0dda12e067e18c9532c2b6b0b6e1cd58227b
AhmedElGamil <ahmed@manhag.org>
2010-10-14 12:21:05 +0200

    Added install manifest

---
+++ manifests/install.pp
@@ -0,0 +1,44 @@
+class asterisk {
+	class install ($version=14) {
+		case $operatingsystem {
+			"CentOS" : {
+				# Installing Asterisk+Digium YUM Repos								
+				file { "/etc/yum.repos.d/centos-asterisk.repo" :
+					ensure => present,
+					owner => root,
+					group => root,
+					mode => 644,
+					source => "puppet://$server/asterisk/centos-asterisk.repo",
+					before => Package["$asterisk_packages"]
+				}
+				file { "/etc/yum.repos.d/centos-digium.repo" :
+					ensure => present,
+					owner => root,
+					group => root,
+					mode => 644,
+					source => "puppet://$server/asterisk/centos-digium.repo",
+					before => Package["$asterisk_packages"]
+				}				
+				
+				# Install asterisk dependcies
+				$asterisk_packages = ["asterisk$version", "asterisk$version-configs", "asterisk$version-addons-mysql"]
+				$mysql_packages = [ "mysql-server", "mysql" ]
+				package { $asterisk_packages: ensure => installed }
+				package { $mysql_packages: ensure => installed }
+			}				
+		}
+
+		service { "asterisk":
+			enable => true,
+			ensure => running,
+			hasrestart => "true",
+			hasstatus => "true",
+			require	=> Package["$asterisk_pacakges"],
+			alias => "asterisk"
+		}
+
+		
+	}
+
+}
+
