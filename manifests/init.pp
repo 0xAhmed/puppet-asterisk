0f630552e1e1afee5b31529813312271b7a56f76
AhmedElGamil <ahmed@manhag.org>
2010-10-17 00:05:55 +0200

    Missing imports

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,8 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
+import "install.pp"
+import "mysql_config.pp"
 class asterisk {
 	include asterisk::install
 	include asterisk::mysql_config
